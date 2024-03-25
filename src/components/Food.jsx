bd14981a40c49debbda342d3d6d49c7c31697ba5
Mocus <mocussmncube@gmail.com>
2024-03-25 21:04:26 +0200

    Adding JS to filter items

--- src/components/Food.jsx
+++ src/components/Food.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { data } from "../data/data";
 
 const Food = () => {
   console.log(data);
+  const [foods, setFoods] = useState(data);
+
   return (
     <div className="max-w-[1640px] m-auto px-4 py-12">
-      <h1 className="text-blue-700 font-bold text-4xl text-center">
-        Top Rated Items
+      <h1 className="text-blue-700 font-bold text-4xl text-center mb-5">
+        TOP RATED iNIT?!
       </h1>
       {/* FILTER ROW */}
       <div className=" flex flex-col lg:flex-row justify-between">
@@ -30,25 +32,33 @@ const Food = () => {
         <p className="font-bold text-gray-700">Filter Price</p>
         <div className="flex justify-between max-w-[300px] w-full">
           <button className="m-1 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white ">
-            $
+            R100
           </button>
           <button className="m-1 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white ">
-            $$$
+            R200
           </button>
           <button className="m-1 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white ">
-            $$$
+            R300
           </button>
-          <button className="m-1 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white ">
+          {/* <button className="m-1 border-blue-700 text-blue-700 hover:bg-blue-700 hover:text-white ">
             $$$$
-          </button>
+          </button> */}
         </div>
       </div>
       {/* DISPLAY FOODS */}
-
-      <div>
-
+      <div className="grid grid-cols-2 lg:grid-cols-4 gap-6 pt-4" >
+       { foods.map((item, index) => (
+          <div key={index} className="border shadow-lg rounded-lg hover:scale-105 duration-500">
+            <img src={item.image} alt={item.name} className="w-full h-[200px] object-cover rounded-t-lg"/>
+            <div className="flex justify-between px-2 py-4">
+              <p className="font-bold">{item.name}</p>
+              <p>
+                <span className="bg-blue-700 text-white p-1 rounded-full">{item.price}</span>
+              </p>
+            </div>
+          </div>
+        ))}
       </div>
-      
     </div>
   );
 };
