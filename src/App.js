e59c7abcb5b3116228cebaf5759ebdfc492501bd
Mocus <mocussmncube@gmail.com>
2024-03-25 18:58:02 +0200

    HeadlineCards added to App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import HeadlineCards from "./components/HeadlineCards";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 
@@ -6,6 +7,7 @@ function App() {
     <>
       <Navbar />
       <Hero />
+      <HeadlineCards />
     </>
   );
 }
